6262c00d61b04abd362b2b6b7b44c6e76c1d822e
shadowelite <alansedp@gmail.com>
2025-05-02 01:21:27 +0530

    clean up

--- hypr/.config/hypr/scripts/wall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sleep 0.5
-echo "$(find ~/wall -type f | shuf -n 1)" > /tmp/walnow
-
-sleep 2
-wal -i $(cat /tmp/walnow)
-swww img $(cat /tmp/walnow)
-pkill -9 wal
--- nwg-panel/.config/nwg-panel/executors/wifi_check.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-if iwgetid | grep -q "ESSID"; then echo "󰤪 "; else echo "󰤩 "; fi
